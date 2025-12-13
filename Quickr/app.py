b54a5069af7046abb093424c534f660d615853f0
Simran Sharma <sssimran.sam@gmail.com>
2025-12-13 13:57:56 +0530

    Quickr predication app code

---
+++ Quickr/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return jsonify(message='Hello from Quickr')
+
+
+if __name__ == '__main__':
+	app.run(debug=True, host='127.0.0.1', port=5000)
